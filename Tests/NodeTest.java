0e79ed26d7d82ea13910d0117fc9a2a0047cd0b8
Jorge.cool <jorge.heredia.13036406@usalesiana.edu.bo>
2023-08-01 13:39:04 -0400

    Insert method added with the split implementation

---
+++ Tests/NodeTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class NodeTest {
+    @Test
+    public void SplitTest() {
+        BTree bTree = new BTree(4);
+        bTree.insert(10);
+        bTree.insert(20);
+        bTree.insert(5);
+        bTree.insert(6);
+        bTree.insert(12);
+        bTree.insert(35);
+        bTree.insert(34);
+        bTree.insert(36);
+        bTree.insert(40);
+
+        System.out.println(bTree);
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,55 @@
+public class Node {
+    private int[] keys;
+    private int order;
+    private Node[] children;
+    private int numKeys;
+    private boolean leaf;
+
+    public Node(int order, boolean leaf) {
+        this.order = order;
+        this.leaf = leaf;
+        this.keys = new int[order - 1];
+        this.children = new Node[order];
+        this.numKeys = 0;
+    }
+
+    public void setChildren(Node[] children) {
+        this.children = children;
+    }
+
+    public void setKeys(int[] keys) {
+        this.keys = keys;
+    }
+
+    public void setLeaf(boolean leaf) {
+        this.leaf = leaf;
+    }
+
+    public void incrementNumKeys() {
+        this.numKeys++;
+    }
+
+    public void setNumKeys(int numKeys) {
+        this.numKeys = numKeys;
+    }
+
+    public int[] getKeys() {
+        return keys;
+    }
+
+    public int getNumKeys() {
+        return numKeys;
+    }
+
+    public Node[] getChildren() {
+        return children;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public boolean isLeaf() {
+        return leaf;
+    }
+}
